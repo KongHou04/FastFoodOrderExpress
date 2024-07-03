f3263c6bb6cfd7b5f5a0fddf36103c05e1eb31fa
CongHau <conghau31052004@gmail.com>
2024-07-03 21:22:31 +0700

    Update repositories (Product, ProductDiscount and ComboDetails). Update and format summaries

--- apis/Restaurant/Repositories/Implements/OrdeRES.cs
+++ apis/Restaurant/Repositories/Implements/OrdeRES.cs
@@ -1,8 +1,52 @@
+using Db.Models;
 using Repositories.Interfaces;
 
 namespace Repositories.Implements;
 
 public class OrderRES : IOrderRES
 {
-    
+    public Order? Add(Order obj)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Order? Cancel(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool? Delete(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Order> Get(int numberOfOrder)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Order? GetById(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Order? GetByUser(Guid userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Order? Update(Order obj, Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Order? UpdateDeliveryStatus(int status, Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Order? UpdatePaymentStatus(int status, Guid id)
+    {
+        throw new NotImplementedException();
+    }
 }
--- apis/Restaurant/Repositories/Implements/OrderDetailsRES.cs
+++ apis/Restaurant/Repositories/Implements/OrderDetailsRES.cs
@@ -1,8 +1,32 @@
+using Db.Models;
 using Repositories.Interfaces;
 
 namespace Repositories.Implements;
 
 public class OrderDetailsRES : IOrderDetailsRES
 {
-    
+    public OrderDetails? Add(OrderDetails obj)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool? Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public OrderDetails? GetById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<OrderDetails> GetByOrder(Guid orderId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public OrderDetails? Update(OrderDetails obj, int id)
+    {
+        throw new NotImplementedException();
+    }
 }
