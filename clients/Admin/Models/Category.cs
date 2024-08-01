1dbca2bf4d75ea37ab07d6c9bef74cb3d3c4b1c1
Toàn <toannguyen030601@gmail.com>
2024-08-01 22:37:39 +0700

    CRUD category

--- clients/Admin/Models/Category.cs
+++ clients/Admin/Models/Category.cs
@@ -5,18 +5,18 @@ namespace admin.Models
     public class Category
     {
         [Key]
-        public Guid Id { get; set; }
+        public Guid Id { get; set; } = Guid.NewGuid();
 
         [Required]
         [StringLength(100)]
-        public string Name { get; set; } = string.Empty;
+        public string? Name { get; set; }
 
         [StringLength(255)]
         public string? Description { get; set; }
 
 
         #region Relationship config
-        public ICollection<Product> Products { get; set; }
+        public ICollection<Product> Products { get; set; } = new List<Product>();
 
         #endregion
     }
